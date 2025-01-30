f7654fe6410ce71a7ab0dd292c201a73a163b987
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2025-01-29 23:18:15 -0300

    fixing outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "bucket_name" {
   description = "Nome do bucket S3 criado"
-  value       = aws_s3_bucket.my_bucket.bucket
+  value       = aws_s3_bucket.bucket_fiap_x.bucket
 }
 
 output "bucket_arn" {
   description = "ARN do bucket S3 criado"
-  value       = aws_s3_bucket.my_bucket.arn
+  value       = aws_s3_bucket.bucket_fiap_x.arn
 }
